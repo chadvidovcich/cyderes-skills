9b00796cd886d3b89534de78c1495c52f03294f2
chadvidovcich <chad.vidovcich@gmail.com>
2022-10-01 16:30:57 -0700

    add testing suite on home routes

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const homeController = require("../controllers/home.js");
+const express = require("express")
+const router = express.Router()
+const homeController = require("../controllers/home.js")
 
 //Main Routes
-router.get("/", homeController.forwardIndex);
-router.get("/api/", homeController.getIndex);
-router.get("/api/:searchQuery", homeController.getIPInfo);
+router.get("/", homeController.forwardIndex)
+router.get("/api/", homeController.getIndex)
+router.get("/api/:searchQuery", homeController.getIPInfo)
 
-module.exports = router;
+module.exports = router
