4db598f9501667a2d488e7144d4a9db7bb3a6a06
chadvidovcich <chad.vidovcich@gmail.com>
2022-09-30 21:27:15 -0700

    manual linting

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let express = require("express")
-let app = express()
+const express = require("express")
+const app = express()
 
-//Import routes
+// Import routes
 const mainRoutes = require("./routes/main.js");
 
-//Body Parsing
+// Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Server Routes
+// Server Routes
 app.use("/",mainRoutes)
 
-//Express Server for Development
+// Express LocalServer for Development
 if (process.env.NODE_ENV === "DEV") {
-  //Use .env file
+  // Use .env file
   require("dotenv").config({ path: "./.env" });
 
-  //Local Server Start
+  // Local Server Start
   app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
   });
